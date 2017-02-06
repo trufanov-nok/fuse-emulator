6ebfc2ef4b0ec86b1be9622b7702e09921b843d7
Sergio Baldoví <serbalgi@gmail.com>
2017-02-06 23:46:50 +0100

    Reduce scope of TRDOS functions

--- peripherals/disk/trdos.h
+++ peripherals/disk/trdos.h
@@ -26,47 +26,7 @@
 
 #include <libspectrum.h>
 
-typedef struct trdos_spec_t {
-  libspectrum_byte first_free_sector; /* 0 to 15 */
-  libspectrum_byte first_free_track;  /* 0 to ? */
-  libspectrum_byte disk_type;
-  libspectrum_byte file_count;
-  libspectrum_word free_sectors;
-  libspectrum_byte id;
-  char password[9];                   /* not null-terminated */
-  libspectrum_byte deleted_files;
-  char disk_label[8];                 /* not null-terminated */
-} trdos_spec_t;
-
-typedef struct trdos_dirent_t {
-  char filename[8];                   /* not null-terminated */
-  char file_extension;
-  libspectrum_word param1;
-  libspectrum_word param2;
-  libspectrum_byte file_length;       /* in sectors */
-  libspectrum_byte start_sector;      /* 0 to 15 */
-  libspectrum_byte start_track;       /* 0 to ? */
-} trdos_dirent_t;
-
-typedef struct trdos_boot_info_t {
-  int have_boot_file;
-  int basic_files_count;
-  char first_basic_file[8];           /* not null-terminated */
-} trdos_boot_info_t;
-
-int
-trdos_read_spec( trdos_spec_t *spec, const libspectrum_byte *src );
-
 void
-trdos_write_spec( libspectrum_byte *dest, const trdos_spec_t *spec );
-
-int
-trdos_read_dirent( trdos_dirent_t *entry, const libspectrum_byte *src );
-
-void
-trdos_write_dirent( libspectrum_byte *dest, const trdos_dirent_t *entry );
-
-int
-trdos_read_fat( trdos_boot_info_t *info, const libspectrum_byte *data );
+trdos_insert_boot_loader( libspectrum_disk_t *d );
 
 #endif                  /* #ifndef FUSE_TRDOS_H */
